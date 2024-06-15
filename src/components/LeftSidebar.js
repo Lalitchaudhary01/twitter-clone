308ffa42ea3c978fd6accc69f2eeeff5f978c63a
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-06-15 09:19:34 +0530

    first commit

---
+++ src/components/LeftSidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftSidebar = () => {
+  return <div>LEftsidebar</div>;
+};
+
+export default LeftSidebar;
---
+++ src/components/RightSidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return <div>RightSIdebar</div>;
+};
+
+export default RightSidebar;
