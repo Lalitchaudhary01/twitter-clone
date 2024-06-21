e51641e81711e595704ae7f35ad4bb96791dcc3a
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-06-21 15:05:31 +0530

    build login's logic with backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,66 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { USER_API_END_POINT } from "../utils/constant";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      // login
+      try {
+        const res = await axios.post(
+          `${USER_API_END_POINT}/login`,
+          { email, password },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+
+        // dispatch(getUser(res?.data?.user));
+        if (res.data.success) {
+          navigate("/");
+          toast.success(res.data.message);
+        }
+      } catch (error) {
+        toast.success(error.response.data.message);
+        console.log(error);
+      }
+    } else {
+      // signup
+      try {
+        const res = await axios.post(
+          `${USER_API_END_POINT}/register`,
+          { name, username, email, password },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+
+        if (res.data.success) {
+          setIsLogin(true);
+          toast.success(res.data.message);
+        }
+      } catch (error) {
+        toast.success(error.response.data.message);
+        console.log(error);
+      }
+    }
+  };
   const loginSignupHandler = () => {
     setIsLogin(!isLogin);
   };
@@ -23,30 +82,38 @@ const Login = () => {
           <h1 className="mt-4 mb-2 text-2xl font-bold">
             {isLogin ? "Login" : "SignUp"}
           </h1>
-          <form className="flex flex-col w-[55%]">
+          <form onSubmit={submitHandler} className="flex flex-col w-[55%]">
             {!isLogin && (
               <>
                 <input
                   type="text"
                   placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
                 />
                 <input
                   type="text"
                   placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
                   className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
                 />
               </>
             )}
 
             <input
-              type="text"
+              type="email"
               placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
             />
             <input
-              type="text"
+              type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
             />
             <button className="bg-[#1D9BF0] border-none py-2 my-4 rounded-full text-lg text-white">
---
+++ src/hooks/useGetUser.js
@@ -0,0 +1,4 @@
+const useGetUser = async () => {
+  try {
+  } catch (error) {}
+};
